7787278687b7bbffce8384e1cd3cafac27cb50f2
LarsBuijs00 <lars@buijsnet.com>
2018-02-23 15:25:26 +0100

    bluescreen

--- Eclipse/src/ProcessingProgramma.java
+++ Eclipse/src/ProcessingProgramma.java
@@ -1,4 +1,4 @@
-package introProcessing;
+
 
 import processing.core.PApplet;
 
@@ -7,5 +7,11 @@ public class ProcessingProgramma extends PApplet{
 	public static void main(String[] args) {
 		PApplet.main(new String[] {"ProcessingProgramma"} );
 	}
-	
+	public void settings() {
+		fullScreen();
+	}
+	public void setup() {
+		fill(120, 120, 255);
+		rect(0, 0, width, height);
+	}
 }
